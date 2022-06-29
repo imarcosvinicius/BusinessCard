3640915f06f4744be696a0fbb0e87815f1acd6ba
Marcos Vinicius Nunes <imarcosvinicius@outlook.com>
2022-06-29 20:51:05 -0300

    Finalizando o layout add card

---
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,17 @@
+package com.example.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.businesscard.R
+import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddBusinessCardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddBusinessCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
